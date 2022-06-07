5bdac068d6454c43ad29a346945faad6da1efac3
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-06-07 21:06:53 +0700

    add require in models

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = (`${__dirname}/../../config/database.js`)[env];
+const config = require(`${__dirname}/../../config/database.js`)[env];
 const db = {};
 
 let sequelize;
@@ -18,7 +18,7 @@ fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = (path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
